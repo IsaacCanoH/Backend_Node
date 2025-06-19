335e432706431c3e1c96a9b133f1004455a1c12e
IsaacCanoH <isac.c.2586@gmail.com>
2025-06-19 16:41:34 -0600

    Server: Av

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ app.use(cors());
 app.use(express.json());
 
 const authRoutes = require('./routes/authRoutes');
-const asistenciaRoutes = require('./routes/asistenciaRoutes');
 const sedeRoutes = require("./routes/sedeRoutes");
+const qrSedeRoutes = require("./routes/qrSedeRoutes");
 
 app.use('/api/auth', authRoutes);
-app.use('/api/asistencia', asistenciaRoutes);
 app.use('/api/sede', sedeRoutes);
+app.use('/api/qrsede',qrSedeRoutes);
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
