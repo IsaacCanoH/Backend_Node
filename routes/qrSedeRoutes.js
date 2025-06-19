c32c4bcbf70ee7d62bb86db451d8f157841b072c
IsaacCanoH <isac.c.2586@gmail.com>
2025-06-19 16:59:58 -0600

    Server: Av

--- routes/qrSedeRoutes.js
+++ routes/qrSedeRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const qrSedeController = require('../controllers/qrSedeController');
 
-router.post('/generar-qr', qrSedeController);
+router.post('/generar-qr', qrSedeController.generarQRSede);
 
 module.exports = router;
