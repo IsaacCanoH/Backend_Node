79177a377db52aa98582bf999ba2d2a3a58cd582
IsaacCanoH <isa.c.2586@gmail.com>
2025-07-25 23:31:06 -0600

    Todo Bien 25

--- index.js
+++ index.js
@@ -16,9 +16,6 @@ app.use(cors({
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
-const upload = multer();
-app.use(upload.none());
-
 // Rutas
 const authRoutes = require('./routes/authRoutes');
 const sedeRoutes = require("./routes/sedeRoutes");
