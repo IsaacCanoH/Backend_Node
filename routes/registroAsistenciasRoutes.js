720d6cada929ab6b62043df08745b1f486fabe25
IsaacCanoH <isa.c.2586@gmail.com>
2025-07-26 21:26:22 -0600

    Todo Bi

--- routes/registroAsistenciasRoutes.js
+++ routes/registroAsistenciasRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const multer = require('multer');
+const upload = multer();
 const registroAsistenciaController = require("../controllers/registroAsistenciasController");
 
-router.post("/registrar-asistencia", registroAsistenciaController.crearAsistencia);
+router.post("/registrar-asistencia",upload.none(), registroAsistenciaController.crearAsistencia);
 router.get("/obtener-asistencia/:usuario_id", registroAsistenciaController.obtenerAsistenciasPorUsuario);
 
 module.exports = router;
